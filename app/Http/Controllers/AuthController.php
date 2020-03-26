9eaeed371b0f1d77bd80537eaa837fb88c747884
GKhelio <gkhelio95@gmail.com>
2020-03-26 17:46:04 +0100

    register and login jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -24,12 +28,31 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function login()
+    public function login(Request $request)
     {
-        $credentials = request(['email', 'password']);
+        // Validate request
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ], [
+            'email.required' => 'El email es requerido',
+            'password.required' => 'La contraseña es requerida',
+        ]);
 
-        if (! $token = auth()->attempt($credentials)) {
+        // We check that the validation is correct
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        if (!$token = auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
             return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $user = auth()->user();
+
+        // We check that the user has validated their email
+        if ($user->email_verified_at == null && $user->hash_email_verified != null) {
+            return response()->json(['error' => 'Email not verified'], 401);
         }
 
         return $this->respondWithToken($token);
@@ -40,12 +63,24 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         // Validate request
         $validator = Validator::make($request->all(), [
-            'email' => 'required|string',
+            'email' => 'required|string|unique:users',
             'phone' => 'required|string',
-            'name' => 'required|string'
+            'name' => 'required|string',
+            'password' => 'required|string|min:8|max:12',
+            'password_confirm' => 'required|string|same:password'
+        ], [
+            'email.required' => 'El email es requerido',
+            'email.unique' => 'El email no introducido ya ha sido registrado',
+            'phone.required' => 'El teléfono es requerido',
+            'name.required' => 'El nombre es requerido',
+            'password.required' => 'La contraseña es requerida',
+            'password.min' => 'La longitud mínima de la contraseña es de 8 caracteres',
+            'password.max' => 'La longitud máxima de la contraseña es de 12 caracteres',
+            'password.same' => 'Las contraseñas no son iguales'
         ]);
 
         // We check that the validation is correct
@@ -53,7 +88,24 @@ class AuthController extends Controller
             return response()->json(['errors' => $validator->errors()], 422);
         }
 
-        return response()->json(['message' => 'Registered Successfully'], 200);
+        // Create user
+        $user = new User();
+        $user->uuid = Str::uuid();
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->name = $request->name;
+        $user->phone = $request->phone;
+        $user->email_verified_at = Carbon::now();
+        //$user->hash_email_verified = Str::uuid();
+        $user->role_id = Role::where('name', 'User')->first()->id;
+
+        //TODO: Enivar el correo de confirmación
+
+        if (!$user->save()) {
+            return response()->json(['message' => 'El usuario no se ha podido registrar correctamente'], 500);
+        }
+
+        return response()->json(['message' => 'Registrado correctamente'], 200);
     }
 
     /**
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
