526883544d9b4cfdb6a392fce3773f61bce8c8dd
GKhelio <gkhelio95@gmail.com>
2020-03-27 16:41:15 +0100

    Update CommunityController.php

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -15,7 +15,7 @@ class CommunityController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('jwt.auth');
+        $this->middleware('jwt.auth', ['except' => ['communities']]);
     }
 
     /**
