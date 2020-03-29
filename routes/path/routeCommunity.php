9e5a38baa255e41e0e58cb39ccb45cc24d502838
GKhelio <gkhelio95@gmail.com>
2020-03-29 22:13:15 +0200

    Update routeCommunity.php

--- routes/path/routeCommunity.php
+++ routes/path/routeCommunity.php
@@ -2,7 +2,7 @@
 
 Route::group(['prefix' => 'communities'], function () {
     Route::get('all', 'CommunityController@communities');
-    Route::get('alias/{alias}', 'CommunityController@alias');
+    Route::get('alias/{alias?}', 'CommunityController@alias');
     Route::post('create', 'CommunityController@create');
     Route::put('update', 'CommunityController@update');
     Route::delete('delete', 'CommunityController@delete');
