c1a18c4626945078a1fe6192e47a71b0894ac247
GKhelio <gkhelio95@gmail.com>
2020-03-30 01:29:15 +0200

    Update InCommunityController.php

--- app/Http/Controllers/InCommunityController.php
+++ app/Http/Controllers/InCommunityController.php
@@ -26,10 +26,19 @@ class InCommunityController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function ranking($alias) {
+    public function ranking(Request $request, $alias = "") {
         // Validate request
-        if ($alias == null) {
-            return response()->json(['errors' => 'El alias no puede ser nulo'], 422);
+        $validator = Validator::make([
+            'alias' => $alias,
+        ],[
+            'alias' => 'required|string',
+        ], [
+            'alias.required' => 'El alias es requerido'
+        ]);
+
+        // We check that the validation is correct
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
         }
 
         $community = Community::where('alias', $alias)->first();
@@ -44,10 +53,19 @@ class InCommunityController extends Controller
             return response()->json(['errors' => 'La comunidad no tiene ningún mak3r'], 404);
         }
 
+        $select = [];
+
+        if (auth()->check()) {
+            $select = ['u.name as name', 'u.uuid as user_uuid', 'sc.units_manufactured as units_manufactured'];
+
+        } else {
+            $select = ['u.name as name', 'sc.units_manufactured as units_manufactured'];
+        }
+
         $ranking = StockControl::from('stock_control as sc')
             ->join('in_community as ic', 'sc.in_community_id', '=', 'ic.id')
             ->join('users as u', 'u.id', '=', 'ic.user_id')
-            ->select('u.name as name', 'u.uuid as user_uuid', 'sc.units_manufactured as units_manufactured')
+            ->select($select)
             ->where('ic.community_id', $community->id)
             ->orderBy('sc.units_manufactured', 'desc')
             ->paginate(15);
