0cfa503e32316d47866f46103c837f8e47827646
GKhelio <gkhelio95@gmail.com>
2020-03-30 01:25:52 +0200

    update alias

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -76,10 +76,19 @@ class CommunityController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function alias($alias) {
+    public function alias(Request $request, $alias = "") {
         // Validate request
-        if ($alias == null) {
-            return response()->json(['errors' => 'El alias es requerido'], 422);
+        $validator = Validator::make([
+            'alias' => $alias,
+        ],[
+            'alias' => 'required|string',
+        ], [
+            'alias.required' => 'El alias es requerido'
+        ]);
+
+        // We check that the validation is correct
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
         }
 
         $checkUser = auth()->check();
--- routes/path/routeCommunity.php
+++ routes/path/routeCommunity.php
@@ -2,11 +2,11 @@
 
 Route::group(['prefix' => 'communities'], function () {
     Route::get('all', 'CommunityController@communities');
-    Route::get('alias/{alias}', 'CommunityController@alias');
+    Route::get('alias/{alias?}', 'CommunityController@alias');
     Route::post('create', 'CommunityController@create');
     Route::put('update', 'CommunityController@update');
     Route::delete('delete', 'CommunityController@delete');
-    Route::get('ranking/{alias}', 'InCommunityController@ranking');
+    Route::get('ranking/{alias?}', 'InCommunityController@ranking');
     Route::post('join', 'UserController@joinCommunity');
     Route::post('piece/add-or-update', 'UserController@addOrUpdatePieceStock');
 });
