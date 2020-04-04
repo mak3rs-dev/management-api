5f07a050d2c966dd9a8d8c3a2b8dd5f9e4ac9918
GKhelio <gkhelio95@gmail.com>
2020-04-04 06:01:32 +0200

    Update PiecesController.php

--- app/Http/Controllers/PiecesController.php
+++ app/Http/Controllers/PiecesController.php
@@ -90,7 +90,7 @@ class PiecesController extends Controller
             ->when($community != null, function ($query) use ($community) {
                 return $query->where('p.community_id', $community->id);
             })
-            ->groupBy('p.id', 'cp.piece_id', 'sc.piece_id')
+            ->groupBy('p.id', 'ic.community_id', 'cp.piece_id', 'sc.piece_id')
             ->paginate(15);
 
         return response()->json($pieces);
