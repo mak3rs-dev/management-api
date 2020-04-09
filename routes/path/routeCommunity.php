edfe13e20d88c28b9e655234f512d71175597ae0
GKhelio <gkhelio95@gmail.com>
2020-04-09 07:02:43 +0200

    Deleted method

--- routes/path/routeCommunity.php
+++ routes/path/routeCommunity.php
@@ -8,7 +8,6 @@ Route::group(['prefix' => 'communities'], function () {
     Route::delete('delete', 'CommunityController@delete');
     Route::get('ranking/{alias?}/{export?}', 'InCommunityController@ranking');
     Route::post('join', 'UserController@joinCommunity');
-    //Route::get('pieces/{alias}', 'PiecesController@piecesOfCommunity');
     Route::post('piece/add-or-update', 'StockControlController@addOrUpdatePieceStock');
     Route::post('collect/add', 'CollectControlController@add');
     Route::put('collect/update', 'CollectControlController@update');
