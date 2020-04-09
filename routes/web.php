b9764a0ca02ea5120ea001efbd60f354441116aa
GKhelio <gkhelio95@gmail.com>
2020-04-09 21:22:00 +0200

    sert url /bot/getupdates in routes web

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ Route::get('/', function () {
     return response()->json(['API MAK3RS']);
 });
 
+Route::post('/bot/getupdates', function() {
+    $updates = Telegram::getUpdates();
+    return (json_encode($updates));
+});
+
 // Example of POST Route:
 Route::post('/<token>/webhook', function () {
     $update = Telegram::commandsHandler(true);
