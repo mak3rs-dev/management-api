8e936b227e560174974dd1b9fd91fd1b435d54ba
GKhelio <gkhelio95@gmail.com>
2020-04-09 20:53:33 +0200

    Create Command start webhook bot telegram

---
+++ app/Console/Commands/BotWebHook.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Telegram\Bot\Laravel\Facades\Telegram;
+use Illuminate\Console\Command;
+
+class BotWebHook extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'telegram:webhook';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $updates = Telegram::getWebhookUpdate();
+        $this->info(var_dump($updates));
+    }
+}
--- app/Console/Commands/Telegram/TelegramStartCommnand.php
+++ app/Console/Commands/Telegram/TelegramStartCommnand.php
@@ -22,16 +22,6 @@ class TelegramStartCommnand extends Command
     protected $description = 'Start Command to get you started';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
