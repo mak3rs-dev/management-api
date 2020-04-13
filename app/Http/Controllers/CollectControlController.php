cc3277f1de6f1036a45a67040125566c89f85085
GKhelio <gkhelio95@gmail.com>
2020-04-13 02:33:15 +0200

    add updated_at in CollectControl

--- app/Http/Controllers/CollectControlController.php
+++ app/Http/Controllers/CollectControlController.php
@@ -120,10 +120,10 @@ class CollectControlController extends Controller
             }
         }
 
-        $select = ['cc.id as id', 'u.name as user_name', 'u.alias as user_alias', 'u.phone as phone', 'ic.mak3r_num as mak3r_num', 'cc.address as collect_address', 'cc.location as collect_location',
-                    'cc.province as collect_province', 'cc.state as collect_state', 'cc.country as collect_country', 'cc.cp as collect_cp',
+        $select = ['cc.id as id', 'u.name as user_name', 'u.alias as user_alias', 'u.phone as phone', 'ic.mak3r_num as mak3r_num', 'cc.address as collect_address',
+                    'cc.location as collect_location', 'cc.province as collect_province', 'cc.state as collect_state', 'cc.country as collect_country', 'cc.cp as collect_cp',
                     'cc.address_description as collect_address_description', 'cc.created_at as created_at', 'st.name as status',
-                    'st.code as status_code', 'u.uuid as user_uuid'];
+                    'st.code as status_code', 'u.uuid as user_uuid', 'cc.updated_at as updated_at'];
 
         $collecControl = CollectControl::select($select)
                         ->from('collect_control as cc')
