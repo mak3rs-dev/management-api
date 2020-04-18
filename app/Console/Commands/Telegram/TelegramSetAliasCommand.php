d12a488644fb7547c319672a7404ed8869309815
ARSA <antonioramonsm@gmail.com>
2020-04-19 00:05:02 +0200

    fix(Telegram/SetAlias): Add unversioned and missing file

--- app/Console/Commands/Telegram/TelegramSetAliasCommand.php
+++ app/Console/Commands/Telegram/TelegramSetAliasCommand.php
@@ -2,50 +2,46 @@
 
 namespace App\Console\Commands\Telegram;
 
-class TelegramStartCommand extends BaseCommand
-{
+class TelegramSetAliasCommand extends BaseCommand {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $name = 'start';
+    protected $name = 'SetAlias';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Start Command to get you started';
+    protected $description = 'Login Command to get you started';
 
     /**
      * Execute the console command.
      *
      * @return mixed
      */
-    public function handle($arguments)
-    {
+    public function handle($arguments) {
         // This will send a message using `sendMessage` method behind the scenes to
         // the user/chat id who triggered this command.
         // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
         // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
 
-        $this->replyWithMessage(['text' => 'Buenas! me llamo Mak3rsManagementBot y te doy la bienvenida!!']);
+        if (parent::isChatType('private')) {
+            $args = explode(' ', $arguments);
+            if (!parent::CheckAuth()) {
+                if (count($args)==2) {
+                    $email = $args[0];
+                    $password = $args[1];
 
-        if ($this->update->getChat()->getType()=="private") {
-            if (!parent::CheckAuth($this->update)) {
-                if ($this->update->getChat()->getUsername()) {
-                    $this->replyWithMessage(['text' => 'Para empezar a interactuar debes de iniciar sesión primero']);
-                    $this->replyWithMessage(['text' => 'Utiliza /login [password]']);
                 } else {
-                    $this->replyWithMessage(['text' => 'Para empezar a interactuar debes de crearte un alias en Ajustes->Perfil->Username, y después ejecutar el siguiente comando']);
                     $this->replyWithMessage(['text' => 'Utiliza /SetAlias [email] [password]']);
                 }
+            } else {
+                $this->replyWithMessage(['text' => 'Ya ha iniciado sesión, lo cual significa que el alias ya era correcto.']);
             }
         }
-
-        // ob_start(); var_dump($e); $text= ob_get_clean();
-        // $this->replyWithMessage(['text' => $text]);
 
         /// This will update the chat status to typing...
         //$this->replyWithChatAction(['action' => Actions::TYPING]);
