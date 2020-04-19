8c217bf9ffb68c25755e8c813b40fee4addc907c
ARSA <antonioramonsm@gmail.com>
2020-04-19 05:33:54 +0200

    feat(Command/KickCommunityNonUsers): Added command

---
+++ app/Console/Commands/KickCommunityNonUsers.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Community;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Telegram\Bot\Exceptions\TelegramSDKException;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+class KickCommunityNonUsers extends Command {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mak3rs:KickCommunityNonUsers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Kick Community Non Users';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle() {
+        $communities = Community::whereRaw("telegram_data REGEXP '.*\"autokicknonuser\":.*true.*'")->get();
+
+        foreach ($communities as $community) {
+            $this->info('Running for '.$community->name);
+
+            $telData = json_decode($community->telegram_data);
+            $kickExcludedIds = [];
+            foreach ($telData->kickExcludedAlias as $item) $kickExcludedIds[] = $item;
+
+            $users = User::select('alias')->whereIn('id', $community->InCommunities->pluck('user_id')->toArray())->get();
+            foreach ($users as $user) {
+                $userTelData = json_encode($user->telegram_data);
+                $kickExcludedIds[] = $userTelData->chatid;
+            }
+
+            $deletedItems = [];
+            foreach ($telData->pendingCheckUsers as $toKickUser) {
+                if (!in_array($toKickUser, $kickExcludedIds)) {
+                    try {
+                        Telegram::kickChatMember([
+                            'chat_id' => $telData->chatid,
+                            'user_id' => $toKickUser
+                        ]);
+                        $deletedItems[] = $toKickUser;
+                    } catch (TelegramSDKException $e) {
+                        ob_start();var_dump($e);$textException=ob_get_clean();
+                        Log::error($textException);
+                    }
+                }
+            }
+
+            foreach ($deletedItems as $item) unset($telData->pendingCheckUsers[$item]);
+
+            $community->telegram_data = json_encode($telData);
+            $community->save();
+        }
+
+    }
+
+}
