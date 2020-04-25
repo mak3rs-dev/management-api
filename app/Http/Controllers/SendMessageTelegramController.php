b4e30208b8db00c414b8b8a02202e61ae2f56066
GKhelio <gkhelio95@gmail.com>
2020-04-25 04:13:43 +0000

    Dev in master

--- app/Http/Controllers/SendMessageTelegramController.php
+++ app/Http/Controllers/SendMessageTelegramController.php
@@ -6,6 +6,7 @@ use App\Models\Community;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use Telegram\Bot\Exceptions\TelegramSDKException;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
@@ -26,7 +27,7 @@ class SendMessageTelegramController extends Controller
      *       mediaType="application/json",
      *       @OA\Schema(
      *         @OA\Property(property="community", description="uuid", type="string"),
-     *         @OA\Property(property="user", description="uuid", type="string"),
+     *         @OA\Property(property="users", description="uuid", type="string"),
      *         @OA\Property(property="message", description="text", type="string"),
      *       ),
      *     ),
@@ -44,11 +45,12 @@ class SendMessageTelegramController extends Controller
         // Validate request
         $validator = Validator::make($request->all(), [
             'community' => 'required|string',
-            'user' => 'required|string',
+            'users' => 'required|array|min:1',
             'message' => 'required|string'
         ], [
             'community.required' => 'La comunidad es requerida',
             'user.required' => 'El usuario es requerido',
+            'user.min' => 'Tiene que haber al menos un usuario',
             'message.required' => 'El mensaje es requerido'
         ]);
 
@@ -57,7 +59,7 @@ class SendMessageTelegramController extends Controller
             return response()->json(['errors' => $validator->errors()], 422);
         }
 
-        $community = Community::where('uuid', $request->community)->first();
+        $community = Community::where('alias', $request->community)->first();
         if ($community == null) {
             return response()->json(['error' => 'No se encuentra la comunidad'], 404);
         }
@@ -68,37 +70,42 @@ class SendMessageTelegramController extends Controller
             return response()->json(['error' => 'No tienes permisos para enviar mensajes'], 403);
         }
 
-        $user = User::where('uuid', $request->user)->first();
-        if ($user == null) {
-            return response()->json(['error' => 'No se encuentra el usuario'], 404);
-        }
+        $users = User::whereIn('uuid', $request->users)->get();
 
-        if (!auth()->user()->hasRole('USER:ADMIN')) {
-            $inCommunityUser = $community->InCommunities->where('user_id', $user->id)->first();
-            if ($inCommunityUser == null) {
-                return response()->json(['error' => 'El Mak3r no pertenece a tu comunidad'], 403);
+        $message = $request->message . "\n\nMensaje enviado por " . (Str::startsWith(auth()->user()->alias, "@") ? auth()->user()->alias : "@".auth()->user()->alias);
+        $errors = [];
+        foreach ($users as $user) {
+            if (!auth()->user()->hasRole('USER:ADMIN')) {
+                $inCommunityUser = $community->InCommunities->where('user_id', $user->id)->first();
+                if ($inCommunityUser == null) {
+                    $errors[] = "El Mak3r $user->alias no pertenece a tu comunidad";
+                }
+            }
+
+            // Parse telegram_data
+            $telData = json_decode($user->telegram_data);
+
+            if ($telData != null && isset($telData->chatid)) {
+                // SendMessage
+                try {
+                    Telegram::sendMessage([
+                        'chat_id' => $telData->chatid,
+                        'text' => $message
+                    ]);
+
+                } catch (TelegramSDKException $e) {
+                    $errors[] = "Al usuario $user->alias no se le ha podido enviar el mensaje";
+                }
+
+            } else {
+               $errors[] = "El usuario $user->alias no tiene un chat_id";
             }
         }
 
-        // Parse telegram_data
-        $telData = json_decode($user->telegram_data);
-
-        if ($telData != null && isset($telData->chatid)) {
-            // SendMessage
-            try {
-                Telegram::sendMessage([
-                    'chat_id' => $telData->chatid,
-                    'text' => $request->message
-                ]);
-
-                return response()->json(['message' => 'El mensaje se ha enviado correctamente'], 200);
-
-            } catch (TelegramSDKException $e) {
-                return response()->json(['error' => 'El mensaje no se ha podido enviar correctamente'], 500);
-            }
-
-        } else {
-            return response()->json(['error' => 'El usuario no tiene asociado un chat_id'], 500);
+        if (count($errors) > 0) {
+            return response()->json(['errors' => $errors], 500);
         }
+
+        return response()->json(['message' => 'El mensaje se ha enviado correctamente'], 200);
     }
 }
